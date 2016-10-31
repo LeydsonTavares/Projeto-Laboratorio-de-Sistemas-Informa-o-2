950ff8d9d4730d1aed049f6c4b9280d80f0d427c
LEYDSON RYAN SILVA TAVARES <001441@CESED.LOCAL>
2016-10-31 09:55:50 -0300

    Adicionando classe BeerDAO

---
+++ app/src/main/java/com/example/leydsontavares/beerregisterapplication/dao/BeerDAO.java
@@ -0,0 +1,162 @@
+package com.example.leydsontavares.beerregisterapplication.dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+
+import com.example.leydsontavares.beerregisterapplication.model.Beer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by LeydsonTavares on 15/04/2016.
+ */
+public class BeerDAO {
+    private Context mContext;
+
+    public BeerDAO(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public void salvarBeers(Beer beer) {
+        ContentValues values = new ContentValues();
+
+        values.put(BeerBD.COLUNAS_TBL_BEER[1], beer.getmNome());
+        values.put(BeerBD.COLUNAS_TBL_BEER[2], beer.getmDescricao());
+        values.put(BeerBD.COLUNAS_TBL_BEER[3], beer.getmImagem());
+        values.put(BeerBD.COLUNAS_TBL_BEER[4], beer.getmQualidade());
+        values.put(BeerBD.COLUNAS_TBL_BEER[5], beer.getmTeorAlcolico());
+        values.put(BeerBD.COLUNAS_TBL_BEER[6], beer.getmNacionalidade());
+
+
+
+        BeerBD.getInstance(mContext).getWritableDatabase().insert(BeerBD.NOME_TBL_BEER, null, values);
+
+
+    }
+
+
+    public void removerBeers(Beer beer) {
+
+        int gerenteId = beer.getmId();
+
+        BeerBD.getInstance(mContext).getWritableDatabase().delete(BeerBD.NOME_TBL_BEER,
+                BeerBD.COLUNAS_TBL_BEER[0] + "=" + gerenteId, null);
+
+
+    }
+
+    public void atualizarBeer(Beer beer) {
+
+        ContentValues values = new ContentValues();
+
+        values.put(BeerBD.COLUNAS_TBL_BEER[1], beer.getmNome());
+        values.put(BeerBD.COLUNAS_TBL_BEER[2], beer.getmDescricao());
+        values.put(BeerBD.COLUNAS_TBL_BEER[3], beer.getmImagem());
+        values.put(BeerBD.COLUNAS_TBL_BEER[4], beer.getmQualidade());
+        values.put(BeerBD.COLUNAS_TBL_BEER[5], beer.getmTeorAlcolico());
+        values.put(BeerBD.COLUNAS_TBL_BEER[6], beer.getmNacionalidade());
+
+
+
+
+        BeerBD.getInstance(mContext).getWritableDatabase().update(BeerBD.NOME_TBL_BEER, values,
+                BeerBD.COLUNAS_TBL_BEER[0] + "=" + beer.getmId(), null);
+
+
+    }
+
+    public Beer buscarBeers(int id) {
+
+        Cursor cursor = BeerBD.getInstance(mContext).getReadableDatabase().query(BeerBD.NOME_TBL_BEER, BeerBD.COLUNAS_TBL_BEER,
+                BeerBD.COLUNAS_TBL_BEER[0] + "=" + id, null, null, null, null);
+        List<Beer> demandasList = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            demandasList.add(cursorBeer(cursor));
+
+        }
+        return demandasList.get(0);
+
+
+    }
+
+    public Beer cursorBeer(Cursor cursor) {
+        Beer beer = new Beer();
+
+        beer.setmId(cursor.getInt(0));
+        beer.setmNome(cursor.getString(1));
+        beer.setmDescricao(cursor.getString(2));
+        beer.setmImagem(cursor.getBlob(3));
+        beer.setmQualidade(cursor.getString(4));
+        beer.setmTeorAlcolico(cursor.getDouble(5));
+        beer.setmNacionalidade(cursor.getString(6));
+
+
+        return beer;
+
+    }
+
+    public List<Beer> BuscarTodos() {
+        Cursor cursor = BeerBD.getInstance(mContext).getReadableDatabase().query(BeerBD.NOME_TBL_BEER,
+                BeerBD.COLUNAS_TBL_BEER,
+                null, null, null, null, null);
+        List<Beer> beers = new ArrayList<>();
+
+        cursor.moveToFirst();
+        while (cursor.moveToNext()) {
+            beers.add(cursorBeer(cursor));
+
+        }
+        return beers;
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
